0cbe3578751db32dc9377cca7f9dbbfa4e018d84
bchwalek <bchwalek@interia.pl>
2021-08-06 00:54:47 +0200

    last changes

--- src/main/java/pl/lightbulb/customer_panel/apisheets/SheetsIdRepository.java
+++ src/main/java/pl/lightbulb/customer_panel/apisheets/SheetsIdRepository.java
@@ -1,8 +1,12 @@
 package pl.lightbulb.customer_panel.apisheets;
 
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SheetsIdRepository extends JpaRepository<SheetsId, Long> {
+
 
 }
